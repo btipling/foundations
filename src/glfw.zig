77e99aaa1af8a4ce136f0ccbf7165803c0f9d1bf
btipling <bjorn@ambientchill.com>
2024-06-02 19:13:05 -0700

    glfw error callback?

--- src/glfw.zig
+++ src/glfw.zig
@@ -6,11 +6,21 @@ const GLFWError = error{
     Fatal,
 };
 
+// void error_callback(int error, const char* description)
+// {
+//     fputs(description, stderr);
+// }
+
+fn errorCallback(err: c_int, description: [*c]const u8) callconv(.C) void {
+    std.log.err("GLFW Error: {d} {s}\n", .{ err, description });
+}
+
 pub fn init() !void {
     if (c.glfwInit() == 0) {
         std.debug.print("could not init glfw", .{});
         return GLFWError.Fatal;
     }
+    _ = c.glfwSetErrorCallback(errorCallback);
     std.debug.print("successfully inited glfw", .{});
 }
 
