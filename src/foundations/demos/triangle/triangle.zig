366e89ef1af058a08bb4569ed4fa1af511424cc6
btipling <bjorn@ambientchill.com>
2024-06-06 22:42:07 -0700

    progress on triangle

--- src/foundations/demos/triangle/triangle.zig
+++ src/foundations/demos/triangle/triangle.zig
@@ -1,5 +1,8 @@
-vao: u32,
 program: u32,
+vao: u32,
+buffer: u32,
+
+const Triangle = @This();
 
 const positions: [3][3]f32 = .{
     .{ 0, 1, 0.5 },
@@ -7,8 +10,22 @@ const positions: [3][3]f32 = .{
     .{ 1, 0, 0.5 },
 };
 
-pub fn init() void {}
+const vertex_shader: []const u8 = @embedFile("vertex.glsl");
+const frag_shader: []const u8 = @embedFile("frag.glsl");
+
+pub fn init() Triangle {
+    const program = rhi.createProgram();
+    rhi.attachShaders(program, vertex_shader, frag_shader);
+    const vao_buf = rhi.attachBuffer(positions[0..]);
+    return .{
+        .program = program,
+        .vao = vao_buf.vao,
+        .buffer = vao_buf.buffer,
+    };
+}
 
 pub fn deinit() void {}
 
 pub fn draw() void {}
+
+const rhi = @import("../../rhi/rhi.zig");
---
+++ src/foundations/demos/triangle/vertex.glsl
@@ -0,0 +1,7 @@
+#version 460 core
+layout (location = 0) in vec3 position;
+
+void main()
+{
+    gl_Position =vec4(position.xyz, 1.0);
+}
