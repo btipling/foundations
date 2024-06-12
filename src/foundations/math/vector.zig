e4fa279c017ddab6a38506c8d4bfbbf41dd3ebfa
btipling <bjorn@ambientchill.com>
2024-06-11 19:21:17 -0700

    added addition and subtraction for vectors

--- src/foundations/math/vector.zig
+++ src/foundations/math/vector.zig
@@ -10,6 +10,7 @@ pub fn mul(m: anytype, v: anytype) @TypeOf(v) {
     switch (@typeInfo(T)) {
         .Vector => |VT| switch (@typeInfo(K)) {
             .Float, .Int, .ComptimeFloat, .ComptimeInt => return v * @as(T, @splat(m)),
+            // This isn't mathmatically correct for vector math, correct vector multiplication is the dot product or the cross product
             .Vector => |VM| if (VT.len != VM.len) @compileError("mismatched vector length") else return v * m,
             else => @compileError("second input must be a vector or scalar"),
         },
@@ -24,7 +25,38 @@ pub fn div(v: anytype, d: anytype) @TypeOf(v) {
     switch (@typeInfo(T)) {
         .Vector => |VT| switch (@typeInfo(K)) {
             .Float, .Int, .ComptimeFloat, .ComptimeInt => return v / @as(T, @splat(d)),
-            .Vector => |VM| if (VT.len != VM.len) @compileError("mismatched vector length") else return v / d,
+            // This isn't mathmatically correct for vector math, vectors can't be divided by other vectors
+            .Vector => |VM| if (VT.len != VM.len) @compileError("mismatched vector dimension") else return v / d,
+            else => @compileError("second input must be a vector or scalar"),
+        },
+        else => {},
+    }
+    @compileError("first input must be a vector");
+}
+
+pub fn add(v1: anytype, v2: anytype) @TypeOf(v1) {
+    const T = @TypeOf(v1);
+    const K = @TypeOf(v2);
+    switch (@typeInfo(T)) {
+        .Vector => |VT| switch (@typeInfo(K)) {
+            // This isn't mathmatically correct for vector math, vectors and scalars cannot be added together
+            .Float, .Int, .ComptimeFloat, .ComptimeInt => return v1 + @as(T, @splat(v2)),
+            .Vector => |VM| if (VT.len != VM.len) @compileError("mismatched vector dimension") else return v1 + v2,
+            else => @compileError("second input must be a vector or scalar"),
+        },
+        else => {},
+    }
+    @compileError("first input must be a vector");
+}
+
+pub fn sub(v1: anytype, v2: anytype) @TypeOf(v1) {
+    const T = @TypeOf(v1);
+    const K = @TypeOf(v2);
+    switch (@typeInfo(T)) {
+        .Vector => |VT| switch (@typeInfo(K)) {
+            // This isn't mathmatically correct for vector math, vectors and scalars cannot be subtracted
+            .Float, .Int, .ComptimeFloat, .ComptimeInt => return v1 - @as(T, @splat(v2)),
+            .Vector => |VM| if (VT.len != VM.len) @compileError("mismatched vector dimension") else return v1 - v2,
             else => @compileError("second input must be a vector or scalar"),
         },
         else => {},
@@ -54,6 +86,24 @@ test div {
     const b: vec4 = .{ 10, -20, 9, 0 };
     const be: vec4 = .{ 1, 2, 3, 0 };
     try std.testing.expectEqual(be, div(b, @as(vec4, .{ 10, -10, 3, 100 })));
+}
+
+test add {
+    const a: vec4 = .{ 1, 2, 3, 0 };
+    const ae: vec4 = .{ 3, 4, 5, 2 };
+    try std.testing.expectEqual(ae, add(a, 2));
+    const b: vec4 = .{ 1, 2, 3, 0 };
+    const be: vec4 = .{ 10, -20, 9, 0 };
+    try std.testing.expectEqual(be, add(@as(vec4, .{ 9, -22, 6, 0 }), b));
+}
+
+test sub {
+    const a: vec4 = .{ 2, 4, 6, 0 };
+    const ae: vec4 = .{ 0, 2, 4, -2 };
+    try std.testing.expectEqual(ae, sub(a, 2));
+    const b: vec4 = .{ 10, -20, 9, 0 };
+    const be: vec4 = .{ 1, 2, 3, 0 };
+    try std.testing.expectEqual(be, sub(b, @as(vec4, .{ 9, -22, 6, 0 })));
 }
 
 const std = @import("std");
