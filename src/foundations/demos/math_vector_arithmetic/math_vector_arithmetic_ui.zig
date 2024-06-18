01e496cee0b671a3890e56381d5a85553bed61f2
btipling <bjorn@ambientchill.com>
2024-06-17 21:50:56 -0700

    some ui

--- src/foundations/demos/math_vector_arithmetic/math_vector_arithmetic_ui.zig
+++ src/foundations/demos/math_vector_arithmetic/math_vector_arithmetic_ui.zig
@@ -1,9 +1,10 @@
-points: [100]math.vector.vec3 = undefined,
+points: [100]math.vector.vec2 = undefined,
 num_points: usize = 0,
+next_vec_data: [2]f32 = .{ 0, 0 },
 
 const vma_ui = @This();
 
-pub fn draw(_: *vma_ui) void {
+pub fn draw(self: *vma_ui) void {
     const btn_dims = ui.helpers().buttonSize();
     const vp: *c.ImGuiViewport = c.igGetMainViewport();
     const pos = c.ImVec2_ImVec2_Float(vp.WorkPos.x + 50, vp.WorkPos.y + 50);
@@ -11,11 +12,19 @@ pub fn draw(_: *vma_ui) void {
     const size = c.ImVec2_ImVec2_Float(550, 680);
     c.igSetNextWindowSize(size.*, c.ImGuiCond_FirstUseEver);
     _ = c.igBegin("Math vector arithmetic", null, 0);
+    _ = c.igInputFloat2("##add", &self.next_vec_data, "%.3f", c.ImGuiInputTextFlags_None);
     if (c.igButton("Add vector", btn_dims)) {
-        std.debug.print("adding a vector\n", .{});
+        std.debug.print("adding a vector from (0, 0) to ({d}, {d})\n", .{
+            self.next_vec_data[0],
+            self.next_vec_data[1],
+        });
+        self.clearInput();
     }
-    c.igText("Math vector arithmetic");
     c.igEnd();
+}
+
+fn clearInput(self: *vma_ui) void {
+    self.next_vec_data = .{ 0, 0 };
 }
 
 const c = @cImport({
