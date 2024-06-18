2f06db8cd9b2720ad553dc48a5c9e97795fbc7b1
btipling <bjorn@ambientchill.com>
2024-06-17 21:39:18 -0700

    button

--- src/foundations/demos/math_vector_arithmetic/math_vector_arithmetic_ui.zig
+++ src/foundations/demos/math_vector_arithmetic/math_vector_arithmetic_ui.zig
@@ -1,12 +1,19 @@
+points: [100]math.vector.vec3 = undefined,
+num_points: usize = 0,
+
 const vma_ui = @This();
 
 pub fn draw(_: *vma_ui) void {
+    const btn_dims = ui.helpers().buttonSize();
     const vp: *c.ImGuiViewport = c.igGetMainViewport();
     const pos = c.ImVec2_ImVec2_Float(vp.WorkPos.x + 50, vp.WorkPos.y + 50);
     c.igSetNextWindowPos(pos.*, c.ImGuiCond_FirstUseEver, c.ImVec2_ImVec2_Float(0, 0).*);
     const size = c.ImVec2_ImVec2_Float(550, 680);
     c.igSetNextWindowSize(size.*, c.ImGuiCond_FirstUseEver);
     _ = c.igBegin("Math vector arithmetic", null, 0);
+    if (c.igButton("Add vector", btn_dims)) {
+        std.debug.print("adding a vector\n", .{});
+    }
     c.igText("Math vector arithmetic");
     c.igEnd();
 }
@@ -17,3 +24,5 @@ const c = @cImport({
 });
 
 const std = @import("std");
+const math = @import("../../math/math.zig");
+const ui = @import("../../ui/ui.zig");
