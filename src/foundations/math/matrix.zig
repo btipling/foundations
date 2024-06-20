50bb35901f925e6dd8d6e3d45cdf3613518552bf
btipling <bjorn@ambientchill.com>
2024-06-19 23:35:29 -0700

    Add a stub for getting a determinant for a matrix

--- src/foundations/math/matrix.zig
+++ src/foundations/math/matrix.zig
@@ -4,3 +4,8 @@ pub fn orthonormalize(self: *matrix) void {
     _ = self;
     @compileError("not yet implemented, see page 32 in book 1 foundations of game engine dev");
 }
+
+pub fn determinant(self: *matrix) void {
+    _ = self;
+    @compileError("not yet implemented, see page 33-34 in book 1 foundatoins of game engine dev");
+}
