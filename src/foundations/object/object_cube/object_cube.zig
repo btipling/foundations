dc68ea4c12efeb6422dc6acb42430db9386d4241
btipling <bjorn@ambientchill.com>
2024-06-25 22:49:47 -0700

    cubes

--- src/foundations/object/object_cube/object_cube.zig
+++ src/foundations/object/object_cube/object_cube.zig
@@ -6,26 +6,66 @@ linear_colorspace: bool = false,
 
 const Quad = @This();
 
-pub const default_positions: [6][3]f32 = .{
-    .{ -1, -1, 0 },
-    .{ -1, 1, 0 },
-    .{ 1, -1, 0 },
+pub const default_positions: [36][3]f32 = .{
+    // z pos
+    .{ -1, -1, 1 },
+    .{ -1, 1, 1 },
+    .{ 1, -1, 1 },
+    .{ -1, 1, 1 },
+    .{ 1, 1, 1 },
+    .{ 1, -1, 1 },
 
-    .{ -1, 1, 0 },
-    .{ 1, 1, 0 },
-    .{ 1, -1, 0 },
+    // z neg
+    .{ -1, -1, -1 },
+    .{ -1, 1, -1 },
+    .{ 1, -1, -1 },
+    .{ -1, 1, -1 },
+    .{ 1, 1, -1 },
+    .{ 1, -1, -1 },
+
+    // y pos
+    .{ -1, 1, -1 },
+    .{ -1, 1, 1 },
+    .{ 1, 1, -1 },
+    .{ -1, 1, 1 },
+    .{ 1, 1, 1 },
+    .{ 1, 1, -1 },
+
+    // y neg
+    .{ -1, -1, -1 },
+    .{ -1, -1, 1 },
+    .{ 1, -1, -1 },
+    .{ -1, -1, 1 },
+    .{ 1, -1, 1 },
+    .{ 1, -1, -1 },
+
+    // x pos
+    .{ 1, -1, -1 },
+    .{ 1, -1, 1 },
+    .{ 1, 1, -1 },
+    .{ 1, -1, 1 },
+    .{ 1, 1, 1 },
+    .{ 1, 1, -1 },
+
+    // x neg
+    .{ -1, -1, -1 },
+    .{ -1, -1, 1 },
+    .{ -1, 1, -1 },
+    .{ -1, -1, 1 },
+    .{ -1, 1, 1 },
+    .{ -1, 1, -1 },
 };
 
 pub fn init(
     vertex_shader: []const u8,
     frag_shader: []const u8,
-    positions: [6][3]f32,
+    positions: [36][3]f32,
     color: [4]f32,
 ) Quad {
     const program = rhi.createProgram();
     rhi.attachShaders(program, vertex_shader, frag_shader);
 
-    var data: [6]rhi.attributeData = undefined;
+    var data: [36]rhi.attributeData = undefined;
     var i: usize = 0;
     while (i < data.len) : (i += 1) {
         data[i] = .{
