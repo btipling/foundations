cc6b420cd33c5346300f616cfd50d86d392e9f9c
btipling <bjorn@ambientchill.com>
2024-07-08 20:03:20 -0700

    Remove unused imports

--- src/foundations/demos/demos.zig
+++ src/foundations/demos/demos.zig
@@ -56,13 +56,3 @@ pub fn drawDemo(self: Demos, frame_time: f64) void {
 const std = @import("std");
 const ui = @import("../ui/ui.zig");
 const demo_type = ui.ui_state.demo_type;
-const point = @import("point/point.zig");
-const point_rotating = @import("point_rotating/point_rotating.zig");
-const triangle = @import("triangle/triangle.zig");
-const triangle_animated = @import("triangle_animated/triangle_animated.zig");
-const math_vector_arithmetic = @import("math_vector_arithmetic/math_vector_arithmetic.zig");
-const linear_colorspace = @import("linear_colorspace/linear_colorspace.zig");
-const cubes_animated = @import("cubes_animated/cubes_animated.zig");
-const circle = @import("circle/circle.zig");
-const sphere = @import("sphere/sphere.zig");
-const line = @import("line/line.zig");
