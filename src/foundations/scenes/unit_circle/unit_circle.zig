76c41d8643a5aa4e47049e44f63e18ec8a352137
btipling <bjorn@ambientchill.com>
2024-07-21 17:43:20 -0700

    drawing a circle

--- src/foundations/scenes/unit_circle/unit_circle.zig
+++ src/foundations/scenes/unit_circle/unit_circle.zig
@@ -1,7 +1,15 @@
+strip: object.object = undefined,
 ui_state: unit_circle_ui,
 allocator: std.mem.Allocator,
 
 const UnitCircle = @This();
+
+const num_triangles: usize = 10_000;
+const num_triangles_f: f32 = @floatFromInt(num_triangles);
+const strip_scale: f32 = 0.005;
+
+const vertex_shader: []const u8 = @embedFile("unit_circle_vertex.glsl");
+const frag_shader: []const u8 = @embedFile("unit_circle_frag.glsl");
 
 pub fn navType() ui.ui_state.scene_nav_info {
     return .{
@@ -11,21 +19,58 @@ pub fn navType() ui.ui_state.scene_nav_info {
 }
 
 pub fn init(allocator: std.mem.Allocator) *UnitCircle {
-    const unitcircle = allocator.create(UnitCircle) catch @panic("OOM");
-    unitcircle.* = .{
+    const unit_circle = allocator.create(UnitCircle) catch @panic("OOM");
+    unit_circle.* = .{
         .ui_state = .{},
         .allocator = allocator,
     };
 
-    return unitcircle;
+    unit_circle.renderCircle();
+
+    return unit_circle;
 }
 
 pub fn deinit(self: *UnitCircle, allocator: std.mem.Allocator) void {
     allocator.destroy(self);
 }
 
+pub fn deleteStrip(self: *UnitCircle) void {
+    var objects: [1]object.object = .{self.strip};
+    rhi.deleteObjects(objects[0..]);
+}
+
+pub fn renderCircle(self: *UnitCircle) void {
+    const program = rhi.createProgram();
+    rhi.attachShaders(program, vertex_shader, frag_shader);
+    var i_datas: [num_triangles]rhi.instanceData = undefined;
+    for (0..num_triangles) |i| {
+        const t: f32 = @floatFromInt(i);
+        const res = math.geometry.parametricCircle(t / num_triangles_f);
+        var m = math.matrix.leftHandedXUpToNDC();
+        m = math.matrix.transformMatrix(m, math.matrix.translate(res[1], 0.0, res[0]));
+        m = math.matrix.transformMatrix(m, math.matrix.uniformScale(strip_scale));
+        const i_data: rhi.instanceData = .{
+            .t_column0 = m.columns[0],
+            .t_column1 = m.columns[1],
+            .t_column2 = m.columns[2],
+            .t_column3 = m.columns[3],
+            .color = .{ 0.5, 0.5, 0.5, 1 },
+        };
+        i_datas[i] = i_data;
+    }
+    const strip: object.object = .{
+        .strip = object.strip.init(
+            program,
+            i_datas[0..],
+        ),
+    };
+    self.strip = strip;
+}
+
 pub fn draw(self: *UnitCircle, _: f64) void {
     self.handleInput();
+    const objects: [1]object.object = .{self.strip};
+    rhi.drawObjects(objects[0..]);
     self.ui_state.draw();
 }
 
@@ -35,8 +80,8 @@ fn handleInput(_: *UnitCircle) void {
 }
 
 const std = @import("std");
-const c = @import("../../c.zig").c;
 const ui = @import("../../ui/ui.zig");
 const rhi = @import("../../rhi/rhi.zig");
+const math = @import("../../math/math.zig");
 const unit_circle_ui = @import("unit_circle_ui.zig");
 const object = @import("../../object/object.zig");
