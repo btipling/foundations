ea5bf402dcb59cb533f4cc372667c46d045392e9
btipling <bjorn@ambientchill.com>
2024-07-21 18:42:04 -0700

    add an explicit sphere function

--- src/foundations/math/geometry.zig
+++ src/foundations/math/geometry.zig
@@ -12,6 +12,11 @@ pub fn implicitCircle(v: vector.vec2, epsilon: ?f32) bool {
     return float.equal(v[0] * v[0] + v[1] * v[1], 1.0, e);
 }
 
+pub fn implicitSphere(center: vector.vec3, radius: f32, point: vector.vec3, epsilon: ?f32) bool {
+    const e = epsilon orelse default_epsilon;
+    return float.equal(vector.magnitude(vector.sub(point, center)), radius, e);
+}
+
 const std = @import("std");
 const vector = @import("vector.zig");
 const float = @import("float.zig");
