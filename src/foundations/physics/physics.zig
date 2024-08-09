5664d4209227f7b230ebd139b70910357bbc2425
btipling <bjorn@ambientchill.com>
2024-08-09 00:43:23 -0700

    integrate

--- src/foundations/physics/physics.zig
+++ src/foundations/physics/physics.zig
@@ -27,16 +27,61 @@ pub inline fn changeInVelocity(acceleration_: f32, dt: f32) f32 {
     return acceleration_ * dt;
 }
 
-pub const time = struct {
-    t: f32 = 0.0,
-    dt: f32 = 1.0,
+pub fn springDampenerAcceleration(s: state, _: f32) f32 {
+    const k: f32 = 15;
+    const b: f32 = 0.1;
+    return -k * s.position - b * s.velocity;
+}
+
+pub const state = struct {
+    position: f32,
+    velocity: f32,
 };
 
-pub const object = struct {
-    velocity: f32 = 0,
-    position: f32 = 0,
-    force: f32 = 10,
-    mass: f32 = 1,
+pub const derivative = struct {
+    dx: f32 = 0,
+    dv: f32 = 0,
 };
+
+pub fn evaluate(initial: state, t: f32, dt: f32, der: derivative) derivative {
+    const s: state = .{
+        .position = initial.position + der.dx * dt,
+        .velocity = initial.velocity + der.dv * dt,
+    };
+    return .{
+        .dx = state.velocity,
+        .dv = springDampenerAcceleration(s, t + dt),
+    };
+}
+
+pub fn integrate(s: state, t: f32, dt: f32) state {
+    const a: derivative = evaluate(s, t, 0, .{});
+    const b: derivative = evaluate(s, t, dt * 0.5, a);
+    const c: derivative = evaluate(s, t, dt * 0.5, b);
+    const d: derivative = evaluate(s, t, dt, c);
+
+    const dxdt: f32 = 1.0 / 6.0 * (a.dx + 2.0 * (b.dx + c.dx) + d.dx);
+    const dvdt: f32 = 1.0 / 6.0 * (a.dv + 2.0 * (b.dv + c.dv) + d.dv);
+
+    return .{
+        .position = state.position + dxdt * dt,
+        .velocity = state.velocity + dvdt * dt,
+    };
+}
+
+pub const step = struct {
+    t: f32,
+    current_time: f32,
+    state: state,
+};
+
+pub fn timestep(s: step, new_time: f32) s {
+    const frame_time = new_time - s.current_time;
+    return .{
+        .t = s.t + frame_time,
+        .state = integrate(s, s.t, frame_time),
+        .current_time = new_time,
+    };
+}
 
 const math = @import("../math/math.zig");
