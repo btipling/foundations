806279b61a6d35121672ce1592e47538910d1405
btipling <bjorn@ambientchill.com>
2024-09-11 23:06:35 -0700

    did the cube and pyramid

--- src/foundations/scenes/cgpoc/chapter4/cube_and_pyramid/CubeAndPyramid.zig
+++ src/foundations/scenes/cgpoc/chapter4/cube_and_pyramid/CubeAndPyramid.zig
@@ -1,15 +1,14 @@
 allocator: std.mem.Allocator,
 pyramid: object.object = .{ .norender = .{} },
+parallelepiped: object.object = .{ .norender = .{} },
 view_camera: *physics.camera.Camera(*CubeAndPyramid, physics.Integrator(physics.SmoothDeceleration)),
-time_uinform: rhi.Uniform = undefined,
 
 const CubeAndPyramid = @This();
 
 const num_cubes = 1;
 
-const transforms: []const u8 = @embedFile("../../../../shaders/transforms.glsl");
-const vertex_main: []const u8 = @embedFile("cube_and_pyramid_vertex_main.glsl");
-const vertex_header: []const u8 = @embedFile("cube_and_pyramid_vertex_header.glsl");
+const pyramid_vertex_shader: []const u8 = @embedFile("pyramid_vertex.glsl");
+const cube_vertex_shader: []const u8 = @embedFile("cube_vertex.glsl");
 const frag_shader: []const u8 = @embedFile("cube_and_pyramid_frag.glsl");
 
 pub fn navType() ui.ui_state.scene_nav_info {
@@ -38,6 +37,7 @@ pub fn init(allocator: std.mem.Allocator, cfg: *config) *CubeAndPyramid {
         .allocator = allocator,
         .view_camera = cam,
     };
+    pd.renderPyramid();
     pd.renderParallepiped();
     return pd;
 }
@@ -49,11 +49,11 @@ pub fn deinit(self: *CubeAndPyramid, allocator: std.mem.Allocator) void {
 }
 
 pub fn draw(self: *CubeAndPyramid, dt: f64) void {
-    self.time_uinform.setUniform1f(@floatCast(dt));
     self.view_camera.update(dt);
     {
-        const objects: [1]object.object = .{
+        const objects: [2]object.object = .{
             self.pyramid,
+            self.parallelepiped,
         };
         rhi.drawObjects(objects[0..]);
     }
@@ -63,21 +63,15 @@ pub fn updateCamera(_: *CubeAndPyramid) void {}
 
 pub fn updatePyramidTransform(_: *CubeAndPyramid, prog: u32) void {
     const m = math.matrix.identity();
-    rhi.setUniformMatrix(prog, "f_cube_transform", m);
+    rhi.setUniformMatrix(prog, "f_transform", m);
 }
 
-pub fn renderParallepiped(self: *CubeAndPyramid) void {
+pub fn renderPyramid(self: *CubeAndPyramid) void {
     const prog = rhi.createProgram();
-    const vertex_shader = std.mem.concat(self.allocator, u8, &[_][]const u8{
-        vertex_header,
-        transforms,
-        vertex_main,
-    }) catch @panic("OOM");
-    defer self.allocator.free(vertex_shader);
-    rhi.attachShaders(prog, vertex_shader, frag_shader);
+    rhi.attachShaders(prog, pyramid_vertex_shader, frag_shader);
     var cm = math.matrix.identity();
-    cm = math.matrix.transformMatrix(cm, math.matrix.translate(0, -1, -1));
-    cm = math.matrix.transformMatrix(cm, math.matrix.scale(2, 1, 1));
+    cm = math.matrix.transformMatrix(cm, math.matrix.translate(5, 4, 3));
+    cm = math.matrix.transformMatrix(cm, math.matrix.scale(1.5, 1, 1));
     const i_data: rhi.instanceData = .{
         .t_column0 = cm.columns[0],
         .t_column1 = cm.columns[1],
@@ -99,7 +93,40 @@ pub fn renderParallepiped(self: *CubeAndPyramid) void {
     self.updatePyramidTransform(prog);
     self.view_camera.addProgram(prog, "f_mvp");
     self.pyramid = pyramid;
-    self.time_uinform = rhi.Uniform.init(prog, "f_tf");
+}
+
+pub fn updateParallepipedTransform(_: *CubeAndPyramid, prog: u32) void {
+    const m = math.matrix.identity();
+    rhi.setUniformMatrix(prog, "f_transform", m);
+}
+
+pub fn renderParallepiped(self: *CubeAndPyramid) void {
+    const prog = rhi.createProgram();
+    rhi.attachShaders(prog, cube_vertex_shader, frag_shader);
+    var i_datas: [1]rhi.instanceData = undefined;
+    {
+        var cm = math.matrix.identity();
+        cm = math.matrix.transformMatrix(cm, math.matrix.translate(0, 0, -1));
+        cm = math.matrix.transformMatrix(cm, math.matrix.uniformScale(2));
+        const i_data: rhi.instanceData = .{
+            .t_column0 = cm.columns[0],
+            .t_column1 = cm.columns[1],
+            .t_column2 = cm.columns[2],
+            .t_column3 = cm.columns[3],
+            .color = .{ 1, 0, 1, 1 },
+        };
+        i_datas[0] = i_data;
+    }
+    const parallelepiped: object.object = .{
+        .parallelepiped = object.Parallelepiped.init(
+            prog,
+            i_datas[0..],
+            false,
+        ),
+    };
+    self.updateParallepipedTransform(prog);
+    self.view_camera.addProgram(prog, "f_mvp");
+    self.parallelepiped = parallelepiped;
 }
 
 const std = @import("std");
--- src/foundations/scenes/cgpoc/chapter4/cube_and_pyramid/cube_vertex.glsl
+++ src/foundations/scenes/cgpoc/chapter4/cube_and_pyramid/cube_vertex.glsl
@@ -9,6 +9,19 @@ layout (location = 6) in vec4 f_t_column3;
 layout (location = 7) in vec4 f_i_color;
 
 uniform mat4 f_mvp;
-uniform float f_tf;
+uniform float f_transform;
 
 out vec4 f_varying_color;
+
+void main()
+{
+    mat4 f_transform = mat4(
+        f_t_column0,
+        f_t_column1,
+        f_t_column2,
+        f_t_column3
+    );
+    vec4 f_pos = f_mvp *  f_transform * vec4(f_position.xyz, 1.0);
+    gl_Position = f_pos;
+    f_varying_color = ((vec4(f_position.xyz, 1.0) * 2.0) - 1.0) * 0.5 + vec4(0.5, 0.5, 0.5, 0.5);
+}
