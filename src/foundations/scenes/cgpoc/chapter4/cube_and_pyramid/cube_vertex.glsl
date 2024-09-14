7234587e7f57fe4e91d36dc1789aa471b058bfb6
btipling <bjorn@ambientchill.com>
2024-09-14 15:45:17 -0700

    Add texture coordinates

--- src/foundations/scenes/cgpoc/chapter4/cube_and_pyramid/cube_vertex.glsl
+++ src/foundations/scenes/cgpoc/chapter4/cube_and_pyramid/cube_vertex.glsl
@@ -2,11 +2,12 @@
 layout (location = 0) in vec3 f_position;
 layout (location = 1) in vec4 f_color;
 layout (location = 2) in vec3 f_normals;
-layout (location = 3) in vec4 f_t_column0;
-layout (location = 4) in vec4 f_t_column1;
-layout (location = 5) in vec4 f_t_column2;
-layout (location = 6) in vec4 f_t_column3;
-layout (location = 7) in vec4 f_i_color;
+layout (location = 3) in vec3 f_texture_coords;
+layout (location = 4) in vec4 f_t_column0;
+layout (location = 5) in vec4 f_t_column1;
+layout (location = 6) in vec4 f_t_column2;
+layout (location = 7) in vec4 f_t_column3;
+layout (location = 8) in vec4 f_i_color;
 
 uniform mat4 f_mvp;
 
--- src/foundations/scenes/shapes/cylinder_animated/ca_vertex.glsl
+++ src/foundations/scenes/shapes/cylinder_animated/ca_vertex.glsl
@@ -2,6 +2,8 @@
 layout (location = 0) in vec3 f_position;
 layout (location = 1) in vec4 f_color;
 layout (location = 2) in vec3 f_normals;
+layout (location = 3) in vec3 f_texture_coords;
+
 
 
 uniform mat4 f_transform;
