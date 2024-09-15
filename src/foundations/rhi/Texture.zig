fc44b20d66e957024065fd2eb5af6490e443e6e5
btipling <bjorn@ambientchill.com>
2024-09-14 18:12:28 -0700

    Support max anisotropy

--- src/foundations/rhi/Texture.zig
+++ src/foundations/rhi/Texture.zig
@@ -25,6 +25,13 @@ pub fn init(image: *assets.Image) Texture {
     );
 
     c.glGenerateTextureMipmap(name);
+    if (c.glfwExtensionSupported("GL_EXT_texture_filter_anisotropic") == 1) {
+        var ansio_setting: f32 = 0;
+        c.glGetFloatv(c.GL_MAX_TEXTURE_MAX_ANISOTROPY, &ansio_setting);
+        std.debug.print("max anisotropy {d}\n", .{ansio_setting});
+        c.glTextureParameterf(name, c.GL_TEXTURE_MAX_ANISOTROPY, ansio_setting);
+    }
+
     t.name = name;
     return t;
 }
@@ -33,5 +40,6 @@ pub fn bind(self: Texture) void {
     c.glBindTextureUnit(self.texture_unit, self.name);
 }
 
+const std = @import("std");
 const c = @import("../c.zig").c;
 const assets = @import("../assets/assets.zig");
