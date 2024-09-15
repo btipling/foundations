ecfc475987b6d50adc6cb52be6dc879f9be7a72a
btipling <bjorn@ambientchill.com>
2024-09-14 18:38:21 -0700

    fix the vec3 texture coordinate

--- src/foundations/scenes/shapes/cylinder_animated/ca_vertex.glsl
+++ src/foundations/scenes/shapes/cylinder_animated/ca_vertex.glsl
@@ -2,7 +2,7 @@
 layout (location = 0) in vec3 f_position;
 layout (location = 1) in vec4 f_color;
 layout (location = 2) in vec3 f_normals;
-layout (location = 3) in vec3 f_texture_coords;
+layout (location = 3) in vec2 f_texture_coords;
 
 
 
