b3089128e17a1e17da2c7fba9def0e5d156e8143
btipling <bjorn@ambientchill.com>
2024-09-16 16:19:06 -0700

    Object object mesh creator

--- src/foundations/object/ObjectObj.zig
+++ src/foundations/object/ObjectObj.zig
@@ -2,32 +2,30 @@ mesh: rhi.Mesh,
 vertex_data_size: usize,
 instance_data_stride: usize,
 
-const Torus = @This();
-const precision: usize = 48;
-const precision_f: f32 = @floatFromInt(precision);
-const num_vertices = (precision + 1) * (precision + 1);
-const num_indices = precision * precision * 6;
-const inner_radius: f32 = 0.5;
-const outer_radius: f32 = 0.2;
+const Obj = @This();
+const max_vertices = 100_000;
+const max_indicies = max_vertices * 3;
 
 pub fn init(
     program: u32,
+    vertices: [][3]f32,
+    texture_coordinates: [][2]f32,
+    normals: [][3]f32,
+    indicies: [][3][3]usize,
     instance_data: []rhi.instanceData,
-    wireframe: bool,
-) Torus {
-    var d = data();
-
-    const vao_buf = rhi.attachInstancedBuffer(d.attribute_data[0..], instance_data);
-    const ebo = rhi.initEBO(@ptrCast(d.indices[0..]), vao_buf.vao);
+) Obj {
+    var d = data(vertices, texture_coordinates, normals, indicies);
+    // The Obj loader creates a vertex for every index currently.
+    const vao_buf = rhi.attachInstancedBuffer(d.attribute_data[0..d.num_indices], instance_data);
+    const ebo = rhi.initEBO(@ptrCast(d.indices[0..d.num_indices]), vao_buf.vao);
     return .{
         .mesh = .{
             .program = program,
             .vao = vao_buf.vao,
             .buffer = vao_buf.buffer,
-            .wire_mesh = wireframe,
             .instance_type = .{
                 .instanced = .{
-                    .index_count = num_indices,
+                    .index_count = d.num_indices,
                     .instances_count = instance_data.len,
                     .ebo = ebo,
                     .primitive = c.GL_TRIANGLES,
@@ -40,87 +38,25 @@ pub fn init(
     };
 }
 
-pub fn updateInstanceAt(self: Torus, index: usize, instance_data: rhi.instanceData) void {
-    rhi.updateInstanceData(self.mesh.buffer, self.vertex_data_size, self.instance_data_stride, index, instance_data);
-}
+fn data(
+    vertices: [][3]f32,
+    texture_coordinates: [][2]f32,
+    normals: [][3]f32,
+    obj_indicies: [][3][3]usize,
+) struct { attribute_data: [max_vertices]rhi.attributeData, indices: [max_indicies]u32, num_indices: usize } {
+    var attribute_data: [max_vertices]rhi.attributeData = undefined;
+    var indices: [max_indicies]u32 = undefined;
 
-fn data() struct { attribute_data: [num_vertices]rhi.attributeData, indices: [num_indices]u32 } {
-    var attribute_data: [num_vertices]rhi.attributeData = undefined;
-    var indices: [num_indices]u32 = undefined;
-
-    const p_index: usize = precision + 1;
-    for (0..p_index) |i| {
-        const i_f: f32 = @floatFromInt(i);
-        const phi: f32 = i_f * std.math.tau / precision_f;
-
-        var m: math.matrix = math.matrix.rotationY(phi);
-        var p1: math.vector.vec4 = .{ outer_radius, 0.0, 0.0, 1.0 };
-        p1 = math.matrix.transformVector(m, p1);
-        const p2: math.vector.vec4 = math.vector.add(p1, @as(math.vector.vec4, .{ 0.0, 0.0, inner_radius, 0.0 }));
-
-        const vertex: [3]f32 = .{ p2[0], p2[1], p2[2] };
-
-        const texture_coords: [2]f32 = .{ 0.0, i_f / precision_f };
-
-        //TODO: support tangents in vertex attributes
-        m = math.matrix.transformMatrix(m, math.matrix.rotationY(phi + (std.math.pi / 2.0)));
-        var tv: math.vector.vec4 = .{ -1.0, 0.0, 0.0, 1.0 };
-        tv = math.matrix.transformVector(m, tv);
-        const t_tangent: math.vector.vec3 = .{ tv[0], tv[1], tv[2] };
-        const s_tangent: math.vector.vec3 = .{ 0, 1, 0 };
-        const normals = math.vector.normalize(math.vector.crossProduct(t_tangent, s_tangent));
+    for (obj_indicies, 0..) |index_data, i| {
         attribute_data[i] = .{
-            .position = vertex,
-            .normals = normals,
-            .texture_coords = texture_coords,
+            .position = vertices[index_data[0]],
+            .normals = normals[index_data[2]],
+            .texture_coords = texture_coordinates[index_data[1]],
         };
+        indices[i] = i;
     }
 
-    for (1..p_index) |ring| {
-        const ring_f: f32 = @floatFromInt(ring);
-        const phi: f32 = ring_f * std.math.tau / precision_f;
-        // float amt = (float)toRadians((float)ring * 360.0f / (prec));
-        const m: math.matrix = math.matrix.rotationX(phi);
-        for (0..p_index) |i| {
-            const index = ring * p_index + i;
-
-            const ad = attribute_data[i];
-            const p = ad.position;
-            var v1: math.vector.vec4 = .{ p[0], p[1], p[2], 0 };
-            v1 = math.matrix.transformVector(m, v1);
-            const vertex: [3]f32 = .{ v1[0], v1[1], v1[2] };
-
-            const texture_coords: [2]f32 = .{ ring_f * 2.0 / precision_f, ad.texture_coords[1] };
-
-            // TODO: support tangents in vertex attributes
-            // s and t tangents need to be created here
-
-            const n = ad.normals;
-            var n1: math.vector.vec4 = .{ n[0], n[1], n[2], 0 };
-            n1 = math.matrix.transformVector(m, n1);
-            const normals: math.vector.vec3 = .{ n1[0], n1[1], n1[2] };
-            attribute_data[index] = .{
-                .position = vertex,
-                .normals = math.vector.normalize(normals),
-                .texture_coords = texture_coords,
-            };
-        }
-    }
-
-    // calculate triangle indices
-    for (0..precision) |ring| {
-        for (0..precision) |i| {
-            const offset = (ring * precision + i) * 2;
-            indices[offset * 3 + 0] = @intCast(ring * (precision + 1) + i);
-            indices[offset * 3 + 2] = @intCast((ring + 1) * (precision + 1) + i);
-            indices[offset * 3 + 1] = @intCast(ring * (precision + 1) + i + 1);
-            indices[(offset + 1) * 3 + 0] = @intCast(ring * (precision + 1) + i + 1);
-            indices[(offset + 1) * 3 + 2] = @intCast((ring + 1) * (precision + 1) + i);
-            indices[(offset + 1) * 3 + 1] = @intCast((ring + 1) * (precision + 1) + i + 1);
-        }
-    }
-
-    return .{ .attribute_data = attribute_data, .indices = indices };
+    return .{ .attribute_data = attribute_data, .indices = indices, .num_indices = obj_indicies.len };
 }
 
 const std = @import("std");
