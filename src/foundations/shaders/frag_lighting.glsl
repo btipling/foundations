851c9d426e27dbc30051e3bc2eb9257de4061969
btipling <bjorn@ambientchill.com>
2024-09-18 21:45:30 -0700

    this works

--- src/foundations/shaders/frag_lighting.glsl
+++ src/foundations/shaders/frag_lighting.glsl
@@ -3,7 +3,22 @@
 in vec4 f_frag_color;
 out vec4 fo_frag_color;
 
+struct Material {
+    vec4 ambient;
+    vec4 diffuse;
+    vec4 specular;
+    float shininess;
+    float padding_1;
+    float padding_2;
+    float padding_3;
+};
+
+layout(std430, binding = 0) buffer MaterialBuffer {
+    Material f_materials[];
+};
+
 void main()
 {
-   fo_frag_color = vec4(1.0, 0.0, 0.0, 1.0);
+    Material f_m = f_materials[0];
+    fo_frag_color = f_m.specular;
 } 
