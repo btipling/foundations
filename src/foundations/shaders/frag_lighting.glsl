18d6b0ab05c215ea15bd0efe8e2e069743c94240
btipling <bjorn@ambientchill.com>
2024-09-18 22:02:47 -0700

    well this works, so now it's just shader code

--- src/foundations/shaders/frag_lighting.glsl
+++ src/foundations/shaders/frag_lighting.glsl
@@ -13,12 +13,33 @@ struct Material {
     float padding_3;
 };
 
+struct Light {
+    vec4 ambient;
+    vec4 diffuse;
+    vec4 specular;
+    vec4 location;
+    vec4 direction;
+    float cutoff;
+    float exponent;
+    float attenuation_constant;
+    float attenuation_linear;
+    float attenuation_quadratic;
+    int light_kind;  // 0: direction, 1: positional, 2: spotlight
+    float padding_1; // Explicit padding
+    float padding_2; // Explicit padding
+};
+
 layout(std430, binding = 0) buffer MaterialBuffer {
     Material f_materials[];
+};
+
+layout(std430, binding = 1) buffer LightBuffer {
+    Light f_lights[];
 };
 
 void main()
 {
     Material f_m = f_materials[0];
-    fo_frag_color = f_m.specular;
-} 
+    Light f_l = f_lights[0];
+    fo_frag_color = f_m.specular + f_l.diffuse;
+}
