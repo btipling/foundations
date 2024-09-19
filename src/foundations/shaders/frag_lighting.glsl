96df6fb09ae3e1e44396eba387466b117308f194
btipling <bjorn@ambientchill.com>
2024-09-18 23:30:46 -0700

    got some lighting I guess

--- src/foundations/shaders/frag_lighting.glsl
+++ src/foundations/shaders/frag_lighting.glsl
@@ -4,7 +4,5 @@ in vec4 f_frag_color;
 
 void main()
 {
-    Material f_m = f_materials[0];
-    Light f_l = f_lights[0];
-    fo_frag_color = f_m.specular + f_l.diffuse;
+    fo_frag_color = f_frag_color;
 }
