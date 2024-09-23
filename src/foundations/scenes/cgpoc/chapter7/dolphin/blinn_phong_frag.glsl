10018b2e0ceec6645973638e26be7533613efaf6
btipling <bjorn@ambientchill.com>
2024-09-22 17:21:02 -0700

    don't overwrite uniform

--- src/foundations/scenes/cgpoc/chapter7/dolphin/blinn_phong_frag.glsl
+++ src/foundations/scenes/cgpoc/chapter7/dolphin/blinn_phong_frag.glsl
@@ -5,6 +5,5 @@ void main()
     Light f_ls[10];
     f_ls[0] = f_lights[0];
     vec4 f_texture_color = texture(f_samp, f_tc);
-    vec4 f_global_ambient = vec4(0.7, 0.7, 0.7, 1.0);
     fo_frag_color = f_texture_color * 0.5 + f_blinn_phong_lighting(f_m, f_ls, 1, f_global_ambient) * 0.5;
 }
