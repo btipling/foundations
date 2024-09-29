27479c2ac5319af462db021fe1d1de748b063085
btipling <bjorn@ambientchill.com>
2024-09-28 20:43:14 -0700

    this is ok too

--- src/foundations/scenes/cgpoc/chapter4/simple_solar_system/blinn_phong_texture_frag.glsl
+++ src/foundations/scenes/cgpoc/chapter4/simple_solar_system/blinn_phong_texture_frag.glsl
@@ -5,5 +5,5 @@ void main()
     f_ls[0] = f_lights[0];
     f_ls[0].direction = vec4(fo_light_dir, 1.0);
     vec4 f_texture_color = texture(f_samp, f_tc);
-    fo_frag_color = f_texture_color * f_blinn_phong_lighting_no_mat(f_ls, 1, vec4(0.0, 0.0, 0.0, 1.0), 32.0);
+    fo_frag_color = f_blinn_phong_lighting_texture(f_texture_color, f_ls, 1,  vec4(0.0, 0.0, 0.0, 1.0), 8.0);
 }
