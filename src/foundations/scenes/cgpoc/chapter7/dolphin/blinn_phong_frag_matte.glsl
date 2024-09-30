b612e75fb298ff854463f3414a1bde677a9a0812
btipling <bjorn@ambientchill.com>
2024-09-30 08:35:15 -0700

    shadows

--- src/foundations/scenes/cgpoc/chapter7/dolphin/blinn_phong_frag_matte.glsl
+++ src/foundations/scenes/cgpoc/chapter7/dolphin/blinn_phong_frag_matte.glsl
@@ -7,8 +7,8 @@ void main()
     f_ls[0] = f_lights[0];
     vec4 f_texture_color = texture(f_samp_1, f_tc);
     float f_not_in_shadow = textureProj(f_shadow_texture, fo_shadow_coord);
-    fo_frag_color = vec4(0.0, 0.0, 0.0, 1.0);
-    if (f_not_in_shadow == 1.0) {
-        fo_frag_color = f_blinn_phong_lighting_texture_matte(f_texture_color, f_ls, 1, f_global_ambient);
+    if (f_not_in_shadow != 1.0) {
+        f_ls[0].ambient = vec4(0.0, 0.0, 0.0, 0.0);
     }
+    fo_frag_color = f_blinn_phong_lighting_texture_matte(f_texture_color, f_ls, 1, f_global_ambient);
 }
