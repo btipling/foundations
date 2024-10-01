81898db4dcd5e437362826e5e4cdcfbc1faca0c8
btipling <bjorn@ambientchill.com>
2024-09-30 21:20:46 -0700

    clear up lighting functions

--- src/foundations/shaders/frag_blinn_phong_lighting.glsl
+++ src/foundations/shaders/frag_blinn_phong_lighting.glsl
@@ -1,4 +1,31 @@
 
+vec4 f_blinn_phong_lighting(Material f_mat, Light f_lights[10], uint num_lights, vec4 f_ambient_light) {
+    num_lights = min(num_lights, 10u);
+
+    vec3 f_V = normalize(f_camera_pos.xyz - fo_vert);
+    vec3 f_N = normalize(fo_normals);
+
+    vec4 rv = vec4(0.0, 0.0, 0.0, 1.0);
+    
+    uint i = 0;
+    do {
+        Light f_light = f_lights[i];
+        vec3 f_L = normalize(f_light.direction.xyz);
+        vec3 f_H = normalize(f_L + f_V).xyz;
+
+        float cosTheta = dot(f_L, f_N);
+        float cosPhi = dot(f_H, f_N);
+
+        vec3 f_ambient = ((f_ambient_light * f_mat.ambient) + (f_light.ambient * f_mat.ambient)).xyz;
+        vec3 f_diffuse = f_light.diffuse.xyz * f_mat.diffuse.xyz * max(cosTheta, 0.0);
+        vec3 f_specular = f_mat.specular.xyz * f_light.specular.xyz * pow(max(cosPhi, 0.0), f_mat.shininess * 4.0);
+
+        rv = rv + vec4((f_ambient + f_diffuse + f_specular), 0.0);
+        i += 1;
+    } while (i < num_lights);
+    return rv;
+}
+
 void main()
 {
     Material f_m = f_materials[0];
