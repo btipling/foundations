42afd2101b41461901f27d6422c97e55c32f7c0a
btipling <bjorn@ambientchill.com>
2024-10-20 20:41:48 -0700

    fix missing y

--- src/foundations/scenes/cgpoc/chapter12/terrain_tessellator/terrain_vert.glsl
+++ src/foundations/scenes/cgpoc/chapter12/terrain_tessellator/terrain_vert.glsl
@@ -10,7 +10,7 @@ void main(void) {
     int f_x = gl_InstanceID % 64;
     int f_y = gl_InstanceID / 64;
 
-    f_tc = vec2( (f_x + f_patch_tex_coords[gl_VertexID].x) / 64.0, (63 - f_y + f_patch_tex_coords[gl_VertexID]) / 64.0);
+    f_tc = vec2( (f_x + f_patch_tex_coords[gl_VertexID].x) / 64.0, (63 - f_y + f_patch_tex_coords[gl_VertexID].y) / 64.0);
 
     gl_Position = vec4(f_tc.x - 0.5, 0.0, (1.0 - f_tc.y) - 0.5, 1.0);
 }
