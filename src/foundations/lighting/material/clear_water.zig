318b495d2e360a7d5346d73c19d2536fe6c3886d
btipling <bjorn@ambientchill.com>
2024-11-02 01:22:08 -0700

    ok some more

--- src/foundations/lighting/material/clear_water.zig
+++ src/foundations/lighting/material/clear_water.zig
@@ -2,7 +2,7 @@ pub const ClearWater: Material = .{
     .ambient = .{ 0.125490, 0.125490, 0.125490, 1 },
     .diffuse = .{ 0.58824, 0.58824, 0.68824, 1 },
     .specular = .{ 0.870588, 0.870588, 0.870588, 1 },
-    .shininess = 100.0,
+    .shininess = 10.0,
 };
 
 const Material = @import("../Material.zig");
