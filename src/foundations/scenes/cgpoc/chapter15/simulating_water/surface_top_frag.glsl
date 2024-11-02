5bd3c845a2f704faf9a7f00bbdc3bee60c3682a2
btipling <bjorn@ambientchill.com>
2024-11-02 16:38:38 -0700

    include the thing I will reuse

--- src/foundations/scenes/cgpoc/chapter15/simulating_water/surface_top_frag.glsl
+++ src/foundations/scenes/cgpoc/chapter15/simulating_water/surface_top_frag.glsl
@@ -15,28 +15,10 @@ uniform vec3 f_waterdata;
 //#include "src/foundations/shaders/camera.glsl"
 //#include "src/foundations/shaders/material.glsl"
 //#include "src/foundations/shaders/light.glsl"
+//#include "src/foundations/scenes/cgpoc/chapter15/simulating_water/surface_wave.glsl"
 
 layout(bindless_sampler) uniform sampler2D f_reflection;
 layout(bindless_sampler) uniform sampler2D f_refraction;
-layout(bindless_sampler) uniform sampler3D f_wave_samp;
-
-vec3 f_estimate_wave_normal(float f_w_offset, float f_w_map_scale, float f_w_h_scale)
-{
-    float f_depth_offset = f_waterdata[1];
-	float f_h1 = 1.0 - (texture(f_wave_samp, vec3(((f_tc.s)) * f_w_map_scale, f_depth_offset, ((f_tc.t) + f_w_offset) * f_w_map_scale))).r * f_w_h_scale;
-	float f_h2 = 1.0 - (texture(f_wave_samp, vec3(((f_tc.s) - f_w_offset) * f_w_map_scale, f_depth_offset, ((f_tc.t) - f_w_offset) * f_w_map_scale))).r * f_w_h_scale;
-	float f_h3 = 1.0 - (texture(f_wave_samp, vec3(((f_tc.s) + f_w_offset) * f_w_map_scale, f_depth_offset, ((f_tc.t) - f_w_offset) * f_w_map_scale))).r * f_w_h_scale;
-	vec3 f_v1 = vec3(0.0, f_h1, -1.0);
-	vec3 f_v2 = vec3(-1.0, f_h2, 1.0);
-	vec3 f_v3 = vec3(1.0, f_h3, 1.0);
-	vec3 f_v4 = f_v2 - f_v1;
-	vec3 f_v5 = f_v3 - f_v1;
-	vec3 f_wn = normalize(cross(f_v4, f_v5));
-	return normalize(vec3(f_wn.y, f_wn.z, f_wn.x));
-    
-    // float f_h1 = texture(f_wave_samp, vec3(0.5, f_tc.t, f_tc.s)).s;
-	// return vec3(f_h1, 0.0, 0.0);
-}
 
 void main()
 {
