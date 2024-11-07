7c3bd8e60c6656f73457e1a77e20bb04ec4564d6
btipling <bjorn@ambientchill.com>
2024-11-06 19:54:26 -0800

    cool nobody cares

--- src/foundations/scenes/cgpoc/chapter16/ray_casting/img_1_compute.glsl
+++ src/foundations/scenes/cgpoc/chapter16/ray_casting/img_1_compute.glsl
@@ -28,15 +28,42 @@ struct Collision {
     int object_index;
 };
 
+Collision f_intersect_sphere_object(Ray f_ray) {
+    Collision f_c;
+    f_c.object_index = 1;
+    f_c.inside = false;
+    vec3 f_p = f_ray.start - f_sphere_position;
+    float f_qa = dot(f_ray.dir, f_ray.dir);
+    float f_qb = dot(2 * f_ray.dir, f_p);
+    float f_qc = dot(f_p, f_p) - f_sphere_radius * f_sphere_radius;
+
+    float f_qd = f_qb * f_qb - 4 * f_qa * f_qc;
+
+    if (f_qd < 0.0) {
+        f_c.t = -1;
+        return f_c;
+    }
+
+    return f_c;
+}
+
 Collision f_get_closest_collision(Ray f_ray) {
-    Collision f_c_col;
+    Collision f_c_col, f_sphere_c;
     f_c_col.object_index = -1;
+
+    f_sphere_c = f_intersect_sphere_object(f_ray);
+
+    if (f_sphere_c.t > 0) {
+        f_c_col = f_sphere_c;
+    }
+
     return f_c_col;
 }
 
 vec3 f_ray_trace(Ray f_ray) {
     Collision f_c = f_get_closest_collision(f_ray);
     if (f_c.object_index == -1) return vec3(0.0);
+    if (f_c.object_index == 1) return f_sphere_color.xyz;
     return f_sphere_color.xyz;
 }
 
